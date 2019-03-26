e5f96f28db8e318127ce911c7ac5d3302e38ebd6
zhoumingjun <zhoumingjun@gmail.com>
2019-03-26 11:36:42 +0800

    add update scripts

--- .bin/dotfiles-update-submodules.sh
+++ .bin/dotfiles-update-submodules.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-}
-dotfiles submodule update --init --recursive
+#!/bin/bash
+dotfiles submodule foreach git checkout master
+dotfiles submodule foreach git pull origin master

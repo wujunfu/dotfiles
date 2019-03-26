b24b1ef2f687b88edebc46b4af9c4aefb831b976
zhoumingjun <zhoumingjun@gmail.com>
2019-03-26 11:32:15 +0800

    update submodule

--- .hyper.js
+++ .hyper.js
@@ -129,6 +129,8 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
+            scrollback: 2147483647,
+
   },
 
   // a list of plugins to fetch and install from npm
@@ -137,7 +139,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [`hyperpower`],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
